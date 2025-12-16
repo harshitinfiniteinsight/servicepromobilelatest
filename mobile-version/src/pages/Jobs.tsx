6a38abcc72140e798783cef5ae2f4cedbf41b3c7
Anjali Agarwal <anjaliagarwal@Anjalis-Air.lan>
2025-12-16 13:24:58 +0530

    fix: Prevent search field from being cut off at top
    Changed padding from py-3 to pt-4 pb-3 to add more top padding and prevent the search field from being cut off by the header

--- mobile-version/src/pages/Jobs.tsx
+++ mobile-version/src/pages/Jobs.tsx
@@ -864,7 +864,7 @@ const Jobs = () => {
       />
 
       {/* Search & Filter Header - Fixed with scrolling content */}
-      <div className="px-4 py-3 bg-background border-b z-10 shadow-sm">
+      <div className="px-4 pt-4 pb-3 bg-background border-b z-10 shadow-sm">
         {/* Search Bar */}
         <div className="relative mb-4">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-500" />
